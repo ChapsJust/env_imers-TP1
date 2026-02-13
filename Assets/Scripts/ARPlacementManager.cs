eed52a13a81ffcc33a5da00ce4c408919218d053
chapsjust <2238424@carrefour.cegepvicto.ca>
2026-02-12 19:21:23 -0500

    Ajout X + O

---
+++ Assets/Scripts/ARPlacementManager.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacementManager : MonoBehaviour
+{
+    [Header("AR Components")]
+    [SerializeField] private ARRaycastManager arRaycastManager;
+    [SerializeField] private ARPlaneManager arPlaneManager;
+
+    [Header("Prefabs")]
+    [SerializeField] private GameObject boardPrefab;
+
+    // État
+    private GameObject placedBoard;
+    private bool boardIsPlaced = false;
+
+    // Input
+    private InputSystem_Actions inputActions;
+    private List<ARRaycastHit> arHits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.AR.Tap.performed += OnTap;
+    }
+
+    void OnDisable()
+    {
+        inputActions.AR.Tap.performed -= OnTap;
+        inputActions.Disable();
+    }
+
+    void OnTap(InputAction.CallbackContext context)
+    {
+        if (boardIsPlaced) return; // Grille déjà placée
+
+        Vector2 screenPosition = inputActions.AR.Point.ReadValue<Vector2>();
+
+        // Raycast AR vers les plans détectés
+        if (arRaycastManager.Raycast(screenPosition, arHits, TrackableType.PlaneWithinPolygon))
+        {
+            Pose hitPose = arHits[0].pose;
+            PlaceBoard(hitPose);
+        }
+    }
+
+    private void PlaceBoard(Pose pose)
+    {
+        placedBoard = Instantiate(boardPrefab, pose.position, pose.rotation);
+        boardIsPlaced = true;
+
+        // Désactiver la visualisation des plans
+        TogglePlaneVisuals(false);
+
+        Debug.Log("Grille placée!");
+    }
+
+    private void TogglePlaneVisuals(bool visible)
+    {
+        foreach (var plane in arPlaneManager.trackables)
+        {
+            plane.gameObject.SetActive(visible);
+        }
+        arPlaneManager.planePrefab = visible ? arPlaneManager.planePrefab : null;
+    }
+
+    public void ResetPlacement()
+    {
+        if (placedBoard != null)
+        {
+            Destroy(placedBoard);
+        }
+        boardIsPlaced = false;
+        TogglePlaneVisuals(true);
+        Debug.Log("Placement réinitialisé");
+    }
+
+    public bool IsBoardPlaced() => boardIsPlaced;
+    public GameObject GetBoard() => placedBoard;
+}
---
+++ Assets/Scripts/CellID.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class CellID : MonoBehaviour
+{
+    //Sert a référencer la cellule dans le script GameManager
+    [SerializeField]
+    private int cellID;
+}
